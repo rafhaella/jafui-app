e3aa376d1c813ed2258083b41f923ac0c05230a3
Rafhaella <rafhaellamoreno@gmail.com>
2023-04-20 23:13:20 -0300

    FileController

--- src/main/java/com/jafui/app/backend_jafui/rest/FileController.java
+++ src/main/java/com/jafui/app/backend_jafui/rest/FileController.java
@@ -1,8 +1,6 @@
 package com.jafui.app.backend_jafui.rest;
 
-import com.jafui.app.backend_jafui.entidades.Place;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
