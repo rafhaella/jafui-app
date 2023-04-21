6b88e3be746165b6d47bed173630b20a81b0c77b
Rafhaella <rafhaellamoreno@gmail.com>
2023-04-20 23:10:57 -0300

    Mudança de metodo para procurar pelo id

--- src/main/java/com/jafui/app/backend_jafui/persistencia/PlaceRepository.java
+++ src/main/java/com/jafui/app/backend_jafui/persistencia/PlaceRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.jafui.app.backend_jafui.entidades.Place;
 
+import java.util.Optional;
+
 
 @EnableScan()
 public interface PlaceRepository extends CrudRepository<Place, String> {
 
-    public Place findByName(String name);
+    public Optional<Place> findById(String id);
 
 }
